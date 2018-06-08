30b02612b6754c9e8b9221a2b33639da8f1f816b
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-06-08 03:18:14 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -3,6 +3,10 @@ export default function createStore(reducer){
   let state;
   let lisetners;
 
+  const subscribe = (listener) => {
+    listeners.push(listener)
+  }
+
   const getState = () => {
     return state
   }
